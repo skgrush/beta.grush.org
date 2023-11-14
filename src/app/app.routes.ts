2909e6529784fc50458f9636cc1de550787498f8
Samuel K. Grush <Samuel@Grush.org>
2023-11-13 19:54:47 -0600

    Add pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/home'),
+    data: {
+      name: 'Home',
+      icon: 'home',
+    },
+  },
+  {
+    path: 'projects',
+    data: {
+      name: 'Projects',
+      icon: 'inventory_2',
+    },
+    children: [
+
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
