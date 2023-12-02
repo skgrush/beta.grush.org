e86da37a4f75d2ec34b28af1f948876957f7e5cd
Samuel K. Grush <Samuel@Grush.org>
2023-12-02 11:02:44 -0600

    Move script to sync-s3 package.

--- scripts/metadata.interface.d.ts
+++
@@ -1,8 +0,0 @@
-
-export interface IMigrateObjectMetadata {
-  readonly CacheControl?: string;
-  readonly ContentType?: string;
-  readonly WebsiteRedirectLocation?: string;
-}
-
-export type IMigrateMetadata = Partial<Record<string, IMigrateObjectMetadata>>;
